ea848a7e348ed9ab05656968de95075c515eeffe
Douglas Marodin <douglasmarodin@gmail.com>
2025-11-11 23:56:03 -0300

    feat: add icon slots to button

--- src/lib/components/Button/button.svelte
+++ src/lib/components/Button/button.svelte
@@ -3,7 +3,19 @@
 	export { className as class };
 </script>
 
-<button type="button" class="sk-button {className}" on:click {...$$restProps}><slot /></button>
+<button type="button" class="sk-button {className}" on:click {...$$restProps}>
+	{#if $$slots['icon-left']}
+	<span class="sk-button__icon__left">
+		<slot name="icon-left" />
+	</span>
+	{/if}
+	<slot />
+	{#if $$slots['icon-right']}
+	<span class="sk-button__icon__right">
+		<slot name="icon-right" />
+	</span>
+	{/if}
+</button>
 
 <style>
 	button.sk-button {
